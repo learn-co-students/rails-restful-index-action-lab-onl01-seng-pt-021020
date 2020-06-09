5c0595a9497bf11333c88d9858a709d6c6415332
Amber Kerawala <amber_kerawala@berkeley.edu>
2020-06-09 22:13:13 +0000

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -4,3 +4,7 @@ class StudentsController < ApplicationController
   end 
 end 
 
+#Create a controller for students and have it inherit from ApplicationController
+#Add a controller action to the StudentsController named index
+#Have the new index action pull in a list of all of the students with Student.all and store it in an instance variable
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
   resources :students, only: :index
+  #Create a route for the path /students that maps to a StudentsController "index" action.
+  
+  #Create an index.html.erb view file and render it from your Students#index action.
 end
