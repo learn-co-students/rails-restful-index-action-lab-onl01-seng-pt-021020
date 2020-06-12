ddfb606b390ab6516d3fdaa206a3e342d158a0ab
pbedrosian <peter_bedrosian@Yahoo.com>
2020-06-12 15:45:07 -0400

    Done.

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,9 @@
+class StudentsController < ActionController::Base
+    # Prevent CSRF attacks by raising an exception.
+    # For APIs, you may want to use :null_session instead.
+    protect_from_forgery with: :exception
+
+    def index
+      @students = Student.all
+    end
+  end
